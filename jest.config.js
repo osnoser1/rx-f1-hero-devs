f06aba1f29c0bba06e9c13daf0b0643351254a0f
Alfonso Andrés López Molina <alfonso.lopez@tile.com>
2023-01-14 15:44:32 -0500

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  globalSetup: 'jest-preset-angular/global-setup',
+};
