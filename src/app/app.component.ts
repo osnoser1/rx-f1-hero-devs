23a4f9d0c47975f61484ecd7d37cbfd1c6e2a1fc
Alfonso Andrés López Molina <osnoser_12@hotmail.com>
2023-01-14 17:43:21 -0500

    refactor: standalone components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
