f458e9db67b79739a9a2e1308271f38dcc0d42f0
Alfonso Andrés López Molina <osnoser_12@hotmail.com>
2023-01-21 17:33:08 -0500

    feat: error handling with retry attempts

--- src/app/core/services/http.service.ts
+++ src/app/core/services/http.service.ts
@@ -1,12 +1,15 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 import { API_URL } from '../tokens';
 import { QueryParamValue } from '../types';
+import { ErrorService } from './error.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HttpService {
+  private readonly error = inject(ErrorService);
   private readonly http = inject(HttpClient);
   private readonly apiUrl = inject(API_URL);
 
@@ -14,6 +17,29 @@ export class HttpService {
     path: string,
     options?: { params?: HttpParams | Record<string, QueryParamValue> },
   ) {
-    return this.http.get<T>(`${this.apiUrl}${path}`, options);
+    return this.http
+      .get<T>(`${this.apiUrl}${path}`, options)
+      .pipe(this.handleError.bind(this));
+  }
+
+  private handleError<T>(obs: Observable<T>) {
+    return obs.pipe(
+      this.error.retry(3),
+      catchError(err => {
+        if (err.error instanceof Error) {
+          // A client-side or network error occurred. Handle it accordingly.
+          console.log('An error occurred:', err.error.message);
+        } else {
+          // The backend returned an unsuccessful response code.
+          // The response body may contain clues as to what went wrong,
+          console.log(
+            `Backend returned code ${err.status}, body was:`,
+            typeof err.error === 'string' ? 'HTML body' : err.error,
+          );
+        }
+
+        return throwError(() => err);
+      }),
+    );
   }
 }
--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,15 +1,48 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { map, Subject } from 'rxjs';
 import { NavListComponent } from './nav-list/nav-list.component';
+import { ErrorService } from '../core/services';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MatSidenavModule, NavListComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatSnackBarModule,
+    MatSidenavModule,
+    NavListComponent,
+  ],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LayoutComponent {}
+export class LayoutComponent implements OnInit, OnDestroy {
+  private readonly errorService = inject(ErrorService);
+  private readonly snackBar = inject(MatSnackBar);
+  private readonly destroy$ = new Subject<void>();
+
+  ngOnInit() {
+    this.errorService.retryWhenError(() =>
+      this.snackBar
+        .open('Error, please try again.', 'Retry', { duration: 3000 })
+        .afterDismissed()
+        .pipe(map(({ dismissedByAction }) => dismissedByAction)),
+    );
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
